4b3018518bc0fbe052bbefa9950ac719ea6dbda6
alex <setis-0@mail.ru>
2018-04-06 01:43:16 +0500

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from "angularfire2/auth";
+import {auth, User} from "firebase";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import GoogleAuthProvider = auth.GoogleAuthProvider;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  user: User | null;
+  lat:number;
+  lng:number;
+  constructor(
+    public service: AngularFireAuth,
+  ) {
+    this.service.authState.subscribe(value => {
+      this.user = value
+    })
+
+  }
+  onSubmit(){
+    this.lat = this.form.value.latitude;
+    this.lng = this.form.value.longitude
+
+  }
+
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      latitude: new FormControl(51.678418, [
+        Validators.required,
+      ]),
+      longitude: new FormControl(7.809007, [
+        Validators.required,
+      ]),
+    });
+
+  }
+
+  login() {
+    this.service.auth.signInWithPopup(new GoogleAuthProvider())
+  }
+  logout() {
+    this.service.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AgmCoreModule} from '@agm/core';
+import {AppComponent} from './app.component';
+import {AngularFireAuthModule} from "angularfire2/auth";
+import {AngularFireModule} from "angularfire2";
+import {CommonModule} from "@angular/common";
 
-
-import { AppComponent } from './app.component';
-
-
+const config = {
+  apiKey: "AIzaSyCemx_kAzwBUFHuI5lIru7FBJH8vSi6yIE",
+  authDomain: "test-2422c.firebaseapp.com",
+  databaseURL: "https://test-2422c.firebaseio.com",
+  projectId: "test-2422c",
+  storageBucket: "test-2422c.appspot.com",
+  messagingSenderId: "290676460340"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule,
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyBC6NmXptsKPp_KJJf9G_fSj8DjwAA_-mc"
+    })
   ],
-  providers: [],
+
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
